18c2489efe35f02af01f372d780003eacefae4a9
Gary Bergeron <garybergjr@gmail.com>
2021-05-12 20:19:06 -0400

    New Crap

---
+++ strings.py
@@ -0,0 +1,10 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + name)
+# If we want a space, we can ad that too
+print(greeting + ' ' + name)
