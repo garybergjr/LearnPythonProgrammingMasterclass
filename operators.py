2e9bd5f3985dd1c9e5f736661578e4098a476246
Gary Bergeron <garybergjr@gmail.com>
2021-05-13 18:43:54 -0400

    operators

---
+++ operators.py
@@ -0,0 +1,22 @@
+a = 12
+b = 3
+
+print(a + b)    # 15
+print(a - b)    # 9
+print(a * b)    # 36
+print(a / b)    # 4.0
+print (a // b)  # 4 integer division, rounded down towards minus infinity
+print(a % b) # 0 modulo: the remainder after integer divison
+
+print()
+
+for i in range(1, 4):
+    print(i)
+
+# This will return an error
+#for i in range(1, a / b):
+#    print(i)
+
+# The two forward slashes will perform integer division and will work
+for i in range(1, a // b):
+    print(i)
--- strings.py
+++ strings.py
@@ -4,7 +4,18 @@ print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "Hello"
-name = input("Please enter your name ")
+name = "Gary"
 print(greeting + name)
+
 # If we want a space, we can ad that too
 print(greeting + ' ' + name)
+
+age = 46
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
