f728327dd34e7e862baf9c7bd3ee713655eb183d
Gary Bergeron <garybergjr@gmail.com>
2021-05-18 22:50:22 -0400

    slices

---
+++ slice.py
@@ -0,0 +1,18 @@
+#         01234567890123
+parrot = "Norwegian Blue"
+
+print(parrot[0:6]) # Norweg.  Starts with 0 index and goes up to but not including positon 6
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9]) # The start value defaults to the start of the sequence
+print(parrot[10:14])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+
+print(parrot[:])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
---
+++ slice2.py
@@ -0,0 +1,8 @@
+#         01234567890123
+parrot = "Norwegian Blue"
+
+print(parrot[0:6]) # Norweg.  Starts with 0 index and goes up to but not including positon 6
+print(parrot[-14:-8])
+
+print(parrot[-4:-2]) # Bl
+print(parrot[-4:12]) # Bl
